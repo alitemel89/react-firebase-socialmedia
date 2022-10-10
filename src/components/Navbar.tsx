f9fe7a66ccd360a7c396aeaddcbf827cc06af4c0
alitemel89 <alitemel89.ce@gmail.com>
2022-10-10 17:56:49 +0300

    update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,22 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 const Navbar = () => {
+  const [user] = useAuthState(auth);
   const [nav, setNav] = useState(false);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+  };
+
   const handleNav = () => {
     setNav(!nav);
   };
+
   return (
     <div className="w-full h-[90px] bg-slate-800">
       <div className="max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full">
@@ -24,11 +34,36 @@ const Navbar = () => {
             <Link to="/about" className="px-4">
               About
             </Link>
-            <Link to="/login">
-              <button className="my-4 flex items-center">
-                Login <FcGoogle className="ml-2" size={25}/>
-              </button>
-            </Link>
+            {user ? (
+              <>
+                <Link to="/about" className="px-4 font-bold text-lg">
+                  {user?.displayName}
+                </Link>
+                <div className="mr-4">
+                  <img
+                    src={user?.photoURL || ""}
+                    className="h-10 w-10 rounded-full"
+                  />
+                </div>
+
+                <Link to="/login">
+                  <button
+                    className="my-4 flex items-center"
+                    onClick={signUserOut}
+                  >
+                    Log out
+                  </button>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <button className="my-4 flex items-center">
+                    Login <FcGoogle className="ml-2" size={25} />
+                  </button>
+                </Link>
+              </>
+            )}
           </ul>
         </div>
 
@@ -42,27 +77,63 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Menu */}
-        <div
-          className={
-            nav
-              ? "w-full bg-slate-800 text-white absolute top-[90px] left-0 flex justify-center text-center"
-              : "absolute left-[-100%]"
-          }
-        >
-          <ul className="flex flex-col md:hidden">
-            <Link to="/" className="py-3">
-              Home
-            </Link>
-            <Link to="/about" className="py-3">
-              About
-            </Link>
-            <Link to="/login">
-              <button className="my-4 flex items-center">
-                Login <FcGoogle className="ml-2"  size={25} />
-              </button>
-            </Link>
-          </ul>
-        </div>
+        {!user ? (
+          <div
+            className={
+              nav
+                ? "w-full bg-slate-800 text-white absolute top-[90px] left-0 flex justify-center text-center"
+                : "absolute left-[-100%]"
+            }
+          >
+            <ul className="flex flex-col md:hidden">
+              <Link to="/" className="py-3">
+                Home
+              </Link>
+              <Link to="/about" className="py-3">
+                About
+              </Link>
+              <Link to="/login">
+                <button className="my-4 flex items-center">
+                  Login <FcGoogle className="ml-2" size={25} />
+                </button>
+              </Link>
+            </ul>
+          </div>
+        ) : (
+          <>
+            <div
+              className={
+                nav
+                  ? "w-full bg-slate-800 text-white absolute top-[90px] left-0 flex justify-center text-center"
+                  : "absolute left-[-100%]"
+              }
+            >
+              <div className="flex flex-col items-center px-4">
+                <span className="text-xl font-bold my-2">
+                  {user?.displayName}
+                </span>
+
+                <img
+                  src={user?.photoURL || ""}
+                  className="h-12 w-12 rounded-full"
+                />
+
+                <Link to="/" className="py-2">Home</Link>
+
+                <Link to="/about" className="py-2">About</Link>
+
+                <Link to="/login">
+                  <button
+                    className="flex items-center my-3"
+                    onClick={signUserOut}
+                  >
+                    Log out
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import heroVid from "../assets/video.mp4";
 
+import { auth, provider } from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 const LoginPage = () => {
+  const navigate = useNavigate()
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result);
+    navigate('/');
+  }
+
   return (
     <div className="flex flex-col justify-center mx-auto w-full h-[90vh] ">
       <video
@@ -12,10 +23,10 @@ const LoginPage = () => {
         muted
       />
       <div className="mx-auto text-center">
-      <h2 className="text-3xl py-4 text-white">
-        Welcome to the Watermelon Joy
-      </h2>
-      <button className="text-white">Sign in with Google</button>
+        <h2 className="text-3xl py-4 text-white">
+          Welcome to the Watermelon Joy
+        </h2>
+        <button onClick={signInWithGoogle} className="text-white">Sign in with Google</button>
       </div>
     </div>
   );
